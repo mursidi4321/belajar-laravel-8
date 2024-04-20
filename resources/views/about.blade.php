d6976ef29fd92f572408ee1035e07099f4e3ae71
mursidi4321 <mursidi4321@gmail.com>
2024-04-21 06:26:47 +0700

    instalasi laravel 8

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Al-mukarom | about-me</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <h1>About me</h1>
+    <h1>Mursidi Omahengendi</h1>
+    <h2>email : mursidi@gmail.com</h2>
+    <img src="img/mursidi.jpg" alt="mursidi">
+
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Al-mukarom | home</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <h1>Selamat datang di AlMukarom Istimewa</h1>
+</body>
+
+</html>
